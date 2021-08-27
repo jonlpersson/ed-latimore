9ff567daec05180eea2071e3d183e6de29f72eb3
rglover <me@ryanglover.net>
2021-08-27 14:26:50 -0500

    add image optimization log to improve performance

--- src/_assets/images.11ty.js
+++ src/_assets/images.11ty.js
@@ -1,57 +1,90 @@
-const path = require('path');
-const util = require('util');
-const glob = require('glob');
-const sharp = require('sharp');
-const mkdirp = require('mkdirp');
-const { options } = require('cssesc');
+const path = require("path");
+const util = require("util");
+const glob = require("glob");
+const sharp = require("sharp");
+const mkdirp = require("mkdirp");
+const { options } = require("cssesc");
+const fs = require("fs");
+const _ = require("lodash");
 
-const outputPath = '_site/assets/images/';
+const outputPath = "_site/assets/images/";
 
 const resizeConf = {
   sizes: [
     {
       width: 320,
-      rename: { suffix: '-320' },
-    }, {
+      rename: { suffix: "-320" },
+    },
+    {
       width: 550,
-      rename: { suffix: '-550' },
-    }
+      rename: { suffix: "-550" },
+    },
   ],
   jpegOptions: {
     quality: 90,
     progressive: true,
     withMetadata: false,
-    force: false
-  //   withoutEnlargement: true,
-  //   errorOnUnusedImage: false,
-  //   errorOnEnlargement: false
+    force: false,
+    //   withoutEnlargement: true,
+    //   errorOnUnusedImage: false,
+    //   errorOnEnlargement: false
   },
   pngOptions: {
     compressionLevel: 8,
-    force: false
-  }
+    force: false,
+  },
 };
 
 module.exports = class {
-
   async data() {
     const filePath = path.join(__dirname, `/images/`);
+
     return {
       permalink: `/assets/images/images.json`,
       eleventyExcludeFromCollections: true,
       filePath,
-    }
+    };
+  }
+
+  readOptimizationLog() {
+    const optimizationLogAsJSON = fs.readFileSync(
+      `${__dirname}/optimized.json`,
+      "utf-8"
+    );
+    const optimizationLog = JSON.parse(optimizationLogAsJSON);
+
+    return optimizationLog || [];
+  }
+
+  updateOptimizationLog(images) {
+    const optimizationLog = this.readOptimizationLog();
+
+    const updatedLog = [
+      ...optimizationLog,
+      ...(images || []).map((image) => {
+        return { image, optimizedAt: new Date().toISOString() };
+      }),
+    ];
+
+    fs.writeFileSync(
+      `${__dirname}/optimized.json`,
+      JSON.stringify(updatedLog, null, 2)
+    );
   }
 
   async loadImages(imgFolder) {
-
+    const classInstance = this;
     const cwd = path.resolve(imgFolder.file);
     const getImages = util.promisify(glob);
+    const optimizationLog = this.readOptimizationLog();
     const processedImages = [];
 
-    const imgs = await getImages('**/*(*.jpg|*.png|*.gif)', { cwd: cwd });
-    imgs.forEach(function (img) {
+    const imgs = await getImages("**/*(*.jpg|*.png|*.gif)", {
+      cwd: cwd,
+      ignore: optimizationLog.map(({ image }) => image),
+    });
 
+    imgs.forEach(function (img) {
       const ext = path.extname(img);
       const base = path.basename(img, ext);
       const dir = path.dirname(img);
@@ -62,15 +95,19 @@ module.exports = class {
       passThroughImg.toFile(path.join(outputPath, dir, base + ext));
 
       resizeConf.sizes.forEach(function (size) {
-        const newPath = path.join(outputPath, dir, base + size.rename.suffix + ext);
+        const newPath = path.join(
+          outputPath,
+          dir,
+          base + size.rename.suffix + ext
+        );
         const image = sharp(imgFolder.file + img);
         image
-          .jpeg( resizeConf.jpegOptions )
-          .png( resizeConf.pngOptions )
+          .jpeg(resizeConf.jpegOptions)
+          .png(resizeConf.pngOptions)
           .resize({
             width: size.width,
             withoutEnlargement: true,
-            kernel: 'lanczos2'
+            kernel: "lanczos2",
           })
           .toFile(newPath);
       });
@@ -78,7 +115,9 @@ module.exports = class {
       processedImages.push(img);
     });
 
-    return JSON.stringify(processedImages, null, '\t');
+    classInstance.updateOptimizationLog(processedImages);
+
+    return JSON.stringify(processedImages, null, "\t");
   }
 
   async render({ filePath }) {
@@ -88,4 +127,4 @@ module.exports = class {
       throw new Error(err);
     }
   }
-}
+};
