8772d75e148c485cfe12dcd72ded55e5f5d9ba67
Jon <jonlpersson@gmail.com>
2021-09-21 15:09:12 +0200

    different cloudcannon config

---
+++ src/_data/cloudcannon.js
@@ -0,0 +1,12 @@
+module.exports = {
+  _collection_groups: [
+    {
+      heading: 'Blogging',
+      collections: ['posts', 'drafts']
+    },
+    {
+      heading: 'Pages',
+      collections: ['products', 'pages']
+    }
+  ]
+}
